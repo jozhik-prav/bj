04ea7b2f6d7d105fda224cff441304ca9dd0451d
Jozhik_Prav <grahova_sasha@mail.ru>
2020-08-07 14:32:05 +0300

    Добавила базу данных

---
+++ BulletJournal/Data/BulletJournalContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BulletJournal.Data
+{
+    public class BulletJournalContext: DbContext
+    {
+        public BulletJournalContext(DbContextOptions<BulletJournalContext> options)
+            : base(options)
+        { 
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Wish> Wishes { get; set; }
+    }
+}
--- BulletJournal/Startup.cs
+++ BulletJournal/Startup.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BulletJournal.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,6 +27,9 @@ namespace BulletJournal
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            string con = "Server=(localdb)\\mssqllocaldb;Database=todobd;Trusted_Connection=True;";
+            // устанавливаем контекст данных
+            services.AddDbContext<BulletJournalContext>(options => options.UseSqlServer(con));
             services.AddControllers();
             services.AddSpaStaticFiles(configuration =>
             {
